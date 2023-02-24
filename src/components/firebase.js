cd9dc6f735ece42cff9bbde6b00634f88c02ca87
YongjuLee <ivan.yongju.lee@gmail.com>
2023-02-24 13:23:42 -0800

    updated firebase

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -10,14 +10,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBXs_T570CyPq1PopX-1Xgb-9OYzEq02ns",
-  authDomain: "myshoppingmall-8c32e.firebaseapp.com",
-  databaseURL: "https://myshoppingmall-8c32e-default-rtdb.firebaseio.com",
-  projectId: "myshoppingmall-8c32e",
-  storageBucket: "myshoppingmall-8c32e.appspot.com",
-  messagingSenderId: "702123191405",
-  appId: "1:702123191405:web:663bd49f6f8679a17ba364",
-  measurementId: "G-CZ4LQ26QQ9",
+  apiKey: "AIzaSyCqHGIImMpOF7UV7llYTzuGiuL5-wANa2I",
+  authDomain: "myshoppingmall-56bfe.firebaseapp.com",
+  projectId: "myshoppingmall-56bfe",
+  storageBucket: "myshoppingmall-56bfe.appspot.com",
+  messagingSenderId: "935650866847",
+  appId: "1:935650866847:web:a529a6ae2eac90fa1fd4aa",
+  measurementId: "G-HN6N62BY7T",
 };
 
 // Initialize Firebase
